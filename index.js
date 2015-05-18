ce182a9cc9d14e317f7409eb2738db75f3116801
Joshua Marshall Moore <lowrekey@gmail.com>
2015-05-17 23:09:02 -0700

    random now shows bob and alice

--- index.js
+++ index.js
@@ -2,92 +2,122 @@
 // Joshua M. Moore
 // April 24th, 2015
 
-fourd = new FourD();
-fourd.init('#display', {width: 600, height: 350});
-var graph = fourd.graph;
-fourd.clear();
-
-var last_bob;
-function bob_and_alice(fourd){
-  var name = Math.random() > 0.5 ? 'Bob' : 'Alice';
-  var texture = Math.random() > 0.5 ? 'img/bob.png' : 'img/alice.png';
-  var bob = fourd.graph.add_vertex({
-    cube: {
-      width: 5,
-      height: 5,
-      depth: 5,
-      texture: texture
-    },
-    label: {
-      text: name,
-      offset: {
-        size: 5,
-        dimension: x,
-        amount: 2,
-        text: name
-      }
-    }
-  });
-
-  if(Math.random() > .5 && last_bob){
-    fourd.graph.add_edge(
-      last_bob,
-      bob
-    );
-  }
-
-  last_bob = bob;
-}
-
-var build_graph;
-var vertices = [];
-function random(fourd, n){
-  if(!n){
-    n = 0;
-    return;
-  }
+(function($){
   
+  fourd = new FourD();
+  fourd.init('#display', {width: 600, height: 350});
+  var graph = fourd.graph;
   fourd.clear();
   
-  var count = 0;
+  function ui_random_graph(){
+    var n = parseInt(prompt('number of vertices, e.g. 250:'));
+    var sane = 250;
+    var continue_anyway = true;
+    if(n > sane){
+      continue_anyway = confirm("This number is considered a little high for most cases, and might eat up your resources, continue anyway? (otherwise we'll stick with " + sane + ".)");
+    }
+    
+    n = continue_anyway ? n : sane;
+    n > 0 ? random(fourd, n) : undefined;
+  }
   
-  build_graph = setInterval(function(){
-    vertices.push(fourd.graph.add_vertex({
+  var build_graph;
+  var vertices = [];
+  
+  function pause(){
+    clearInterval(build_graph);
+  }
+  
+  function random(fourd, n){
+    if(!n){
+      n = 0;
+      return;
+    }
+    
+    fourd.clear();
+    
+    var count = 0;
+    build_graph = setInterval(function(){
+      vertices.push(fourd.graph.add_vertex({
+        cube: {
+          width: 5,
+          height: 5,
+          depth: 5,
+          texture: Math.random() >= 0.5 ? 'img/bob.png' : 'img/alice.png'
+        }
+        /*,
+        label: {
+          text: count.toString(),
+        }
+        */
+      }));
+      
+      var source = fourd.graph.V[Math.floor(Math.random() * Object.keys(fourd.graph.V).length)];
+      var target = fourd.graph.V[Math.floor(Math.random() * Object.keys(fourd.graph.V).length)];
+      
+      if(count > 2){
+        fourd.graph.add_edge(source, target);
+      }
+      
+      if(count++ >= n){
+        pause();
+      }
+    }, 25);
+  }
+  
+  random(fourd, 25);
+  $('#random-graph').click(ui_random_graph);
+  $('#pause').click(pause);
+  $('#clear').click(clear);
+  
+  var last_bob;
+  function bob_and_alice(fourd){
+    var name = Math.random() > 0.5 ? 'Bob' : 'Alice';
+    var texture = Math.random() > 0.5 ? 'img/bob.png' : 'img/alice.png';
+    var bob = fourd.graph.add_vertex({
       cube: {
         width: 5,
         height: 5,
         depth: 5,
-        texture: Math.random() >= 0.5 ? 'img/bob.png' : 'img/alice.png'
-      }
-      /*,
+        texture: texture
+      },
       label: {
-        text: count.toString(),
+        text: name,
+        offset: {
+          size: 5,
+          dimension: x,
+          amount: 2,
+          text: name
+        }
       }
-      */
-    }));
-    
-    var source = fourd.graph.V[Math.floor(Math.random() * Object.keys(fourd.graph.V).length)];
-    var target = fourd.graph.V[Math.floor(Math.random() * Object.keys(fourd.graph.V).length)];
-    
-    if(count > 2){
-      fourd.graph.add_edge(source, target);
-    }
-    
-    if(count++ >= n){
-      clearInterval(build_graph);
-    }
-  }, 25);
-}
+    });
 
-function collect_random(){
-  var i = 0;
-  var collect_graph = setInterval(function(){
-    var v = fourd.graph.V[Math.floor(Math.random() * Object.keys(fourd.graph.V).length)];
-    fourd.graph.remove_vertex(vertices[i]);
-    if(i++ === vertices.length - 1){
-      clearInterval(collect_graph);
+    if(Math.random() > .5 && last_bob){
+      fourd.graph.add_edge(
+        last_bob,
+        bob
+      );
     }
-  }, 25);
-}
 
-random(fourd, 25);
+    last_bob = bob;
+  }
+
+
+  function collect_random(n){
+    var i = 0;
+    var collect_graph = setInterval(function(){
+      var v = fourd.graph.V[Math.floor(Math.random() * Object.keys(fourd.graph.V).length)];
+      fourd.graph.remove_vertex(vertices[i]);
+      i += 1;
+      if(i < n || i < vertices.length){
+        clearInterval(collect_graph);
+      }
+    }, 25);
+  }
+  
+})(jQuery);
+
+
+
+
+
